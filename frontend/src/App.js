ca23d29c80ecac103bba225e28fc2922d1df08dc
pashorg <pashorg@yandex.ru>
2020-06-14 19:35:59 +0500

    try chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import  React, { Component } from  'react';
 import { BrowserRouter } from  'react-router-dom'
-import { Route, Link } from  'react-router-dom'
+import { Route } from  'react-router-dom'
 import CityList  from  './CityList'
 import City from './City'
 import  './App.css';
--- frontend/src/City.js
+++ frontend/src/City.js
@@ -1,8 +1,10 @@
 import React, { Component } from  'react';
+import HoursGraph from './HoursCityGraph'
 
 import WeatherService  from  './WeatherService';
 
 const weatherService  =  new  WeatherService();
+
 
 
 class City extends Component {
@@ -11,8 +13,18 @@ class City extends Component {
         super(props);
         this.state  = {
             city: {},
-	    weather: {},
+            weather_common: {},
+            weather_current: {},
+            time_stamp: '',
         };
+    }
+
+    unixTimeToLocal(unixTime) {
+        var date = new Date(unixTime);
+        var dateStr = date.getUTCFullYear() + '-' + (date.getUTCMonth() + 1) + '-' + date.getUTCDate();
+        var timeStr = date.getUTCHours() + ":" + date.getUTCMinutes() + ":" + date.getUTCSeconds();
+        var dateTime = dateStr + ' ' + timeStr + ' UTC';
+        return dateTime;
     }
 
     componentDidMount() {
@@ -23,45 +35,73 @@ class City extends Component {
             self.setState({ city: result.data});
 
             weatherService.getWeather( result.data.latitude, result.data.longitude ).then(function (weather_result) {
+                console.log( weather_result );
                 self.setState({ 
-//                        weather: {
-//                                ...weather_result,
-//                                current: {
-//                                   ...weather_result.current
-//                                }
-//                        }
-                        weather: JSON.parse(JSON.stringify(weather_result))                      
+                    weather_common: weather_result,
+                    weather_current: weather_result.current
                 })
             });
-
         });
     }
     
     render() {
-        console.log(this.state.weather);
         return (
         <div className="City--Single">
             <div className="row">
-	        <div className="col-sm-12">
+	            <div className="col-sm-12">
                     <h1>{this.state.city.country} - {this.state.city.name}</h1>
                     <h2>Current Weather</h2>
                     <table>
                     <tbody>
                         <tr>
-                            <td className="col-sm-6">
-                                Time
+                            <td>
+                                Current Time
                             </td>
-                            <td className="col-sm-6">
-                                {this.state.weather.current.dt}
+                            <td>
+                                { this.unixTimeToLocal( this.state.weather_current.dt * 1000 ) }
+                            </td>
+                        </tr>
+                        <tr>
+                            <td >
+                                Sunrise
+                            </td>
+                            <td >
+                                { this.unixTimeToLocal( this.state.weather_current.sunrise * 1000 ) }
+                            </td>
+                        </tr>
+                        <tr>
+                            <td >
+                                 Sunset
+                            </td>
+                            <td >
+                                { this.unixTimeToLocal( this.state.weather_current.sunset * 1000 ) }
+                            </td>
+                        </tr>
+                        <tr>
+                            <td >
+                                Temperature
+                            </td>
+                            <td >
+                                { (this.state.weather_current.temp - 273).toFixed(2) + ' C' }
+                            </td>
+                        </tr>
+                        <tr>
+                            <td >
+                                Feels like
+                            </td>
+                            <td >
+                                { (this.state.weather_current.feels_like - 273).toFixed(2) + ' C' }
                             </td>
                         </tr>
                     </tbody>
-		    </table>
+                    </table>
+                </div>
+                <div id="HoursGraph" className="col-sm-12">
+                    <HoursGraph />
                 </div>
             </div>
         </div>
         );
     }
-
 }
 export default City;
